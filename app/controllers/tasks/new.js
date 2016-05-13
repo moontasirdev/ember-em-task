576ec1792550c6083813e399a5b0c3b107ec1d0f
m00ntasir <moontasir.dev@outlook.com>
2016-05-13 00:35:20 -0400

    add new controller

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -3,9 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         addTask: function() {
-            debugger;
             var title = this.get('title');
-            alert(title);
         }
     }
 });
