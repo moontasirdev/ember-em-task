d1c307c6a71011efa64c639fce316bf3b39ccf61
m00ntasir <moontasir.dev@outlook.com>
2016-05-15 22:44:04 -0400

    Load data from firebase

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+import Model from 'ember-data/model';
+import DS from 'ember-data';
 
 export default Ember.Route.extend({
+    model: function () {
+        return this.store.findAll('task');
+    }
 });
